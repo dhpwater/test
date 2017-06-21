43016ad9ceaa6455d10ff8595dc748276f02d462
ryan <ryan@daihaipeng>
2017-06-21 17:24:59 +0800

    增加注释

--- src/main/java/com/ns/util/HttpClient.java
+++ src/main/java/com/ns/util/HttpClient.java
@@ -1,5 +1,11 @@
 package com.ns.util;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+
 public class HttpClient {
 
 	public static void main(String[] args) {
@@ -7,6 +13,17 @@ public class HttpClient {
 
 		System.out.println("test case");
 		
+		List<String> lists = null;
+        try {  
+            if(!(new File("persons.txt").exists())){
+            	System.exit(0);
+            }
+            lists = FileUtils.readLines(new File("persons.txt"), "utf-8");
+        } catch (IOException e) {  
+             System.out.println(e);
+        }  
+        
+        System.out.println(lists);
 	}
 
 }
